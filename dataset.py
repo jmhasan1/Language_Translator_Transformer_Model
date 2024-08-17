8e9a077bace5413e8770350d27814f4aed1f0c5e
jmhasan1 <jmhasan17@gmail.com>
2024-08-18 02:57:41 +0530

    updated dataset.py & train.py

---
+++ dataset.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+class BilingualDataset(Dataset):
+    def __init__(self , ds , tokenizer_src , tokenizer_tgt , src_lang , tgt_lang , seq_len ) -> None:
+        self.ds = ds
+        self.tokenizer_src = tokenizer_src
+        self.tokenizer_tgt = tokenizer_tgt
+        self.src_lang = src_lang
+        self.tgt_lang = tgt_lang
+
+        self.sos_token = torch.Tensor([tokenizer_src.token_to_id('[SOS]')] , dtype=torch.int64)
+        self.eos_token = torch.Tensor([tokenizer_src.token_to_id('[EOS]')] , dtype=torch.int64)
+        self.pad_token = torch.Tensor([tokenizer_src.token_to_id('[POS]')] , dtype=torch.int64)
+    
+    def __len__(self):
+        return len(self.ds)
+    
+    def __getitem__(self, index : Any) -> Any:
+        src_target_pair = self.ds[index]
+        src_text = src_target_pair['translation'][self.sec_lang]
+        tgt_text = src_target_pair['translation'][self.tgt_lang]
+
+        enc_input_tokens = self.tokenizer_src.encode(src_text).ids
+        dec_input_tokens = self.tokenizer_tgt.encode(tgt_text).ids
+
+        # how many padding we need to reach the sequence length
+        enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2
+        dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) - 1
+
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0 :
+            raise ValueError("The sequence length exceeds the maximum allowed length.")
+        
+        # Add <s> and </s> token
+        encoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(enc_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token] * enc_num_padding_tokens, dtype=torch.int64),
+            ],
+            dim=0,
+        )
+        
+        # # Add only <s> token
+        decoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64),
+
+            ]
+        )
+
+        # Add only </s> token
+        label = torch.cat(
+            [
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64),
+            ]
+        )
+
+        # Checking 
+        assert encoder_input.size(0) == self.seq_len
+        assert decoder_input.size(0) == self.seq_len
+        assert label.size(0)  == self.seq_len
+
+        return {
+            'encoder_input': encoder_input,  #(Seq_len)
+            'decoder_input': decoder_input,  #(Seq_len)
+            'encoder_mask' : (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int() , #(1 ,1 , seq_len)
+            'decoder_mask': (decoder_input != self.pad_token).unsqueeze(0).int() & causal_mask(decoder_input.size(0)) , #(1 , seq_len) & (1 , seq_len , seq_len)
+            'label': label,
+            'src_text': src_text,
+            'tgt_text': tgt_text.
+        }
+    
+    def causal_mask(size):
+        mask = torch.triu(torch.ones((1, size, size)), diagonal=1).type(torch.int)
+        return mask == 0
+
+
+    
+
+        
+    
+
+
+
+
+       
--- train.py
+++ train.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset , DataLoader , random_split
 
+from dataset import BilingualDataset , causal_mask
+
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
@@ -43,6 +45,20 @@ def get_ds(config):
     train_ds_raw , val_ds_raw = random_split(ds_raw , [train_ds_size , val_ds_size])
     # random_split (Pytorch) allows to split dataset using the size given as input
 
-    
+    train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+
+     # Find the maximum length of each sentence in the source and target sentence
+    max_len_src = 0
+    max_len_tgt = 0
+
+    for item in ds_raw:
+        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+
+    print(f'Max length of source sentence: {max_len_src}')
+    print(f'Max length of target sentence: {max_len_tgt}')
 
 
